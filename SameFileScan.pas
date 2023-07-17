eb7149985aa9f93f6fe9061ea0d47fd06cfaca03
gzip4 <gzip4ever@gmail.com>
2023-07-17 19:45:14 +0500

    Fix typos

--- SameFileScan.pas
+++ SameFileScan.pas
@@ -10,9 +10,10 @@ type
     Hash: string;
     Filesize: string;
   end;
+
   TFileInfoList = TList<TFileInfo>;
 
-function ScanDirectory(path: string): TFileInfoList;
+function ScanDirectory(Path: string): TFileInfoList;
 
 implementation
 
@@ -29,7 +30,7 @@ var
   Dir: string;
   TempFiles: TDictionary<TFileSize, TFileNames>;
   Names: TFileNames;
-  FileSize: TFileSize;
+  Filesize: TFileSize;
   I: Integer;
 begin
   Result := TFileInfoList.Create;
@@ -38,45 +39,46 @@ begin
   TempFiles := TDictionary<TFileSize, TFileNames>.Create;
   try
     if FindFirst(Dir + '\*', faAnyFile, Rec) = 0 then
-    try
-      repeat
-        if (Rec.Name = '.') or (Rec.Name = '..') then
-          continue;
-        if (Rec.Attr and faVolumeID) = faVolumeID then
-          continue; // nothing useful to do with volume IDs
-        if (Rec.Attr and faHidden) = faHidden then
-          continue; // honor the OS "hidden" setting
-        if (Rec.Attr and faDirectory) = faDirectory then
-          continue; // This is a directory. Might want to do something special.
+      try
+        repeat
+          if (Rec.Name = '.') or (Rec.Name = '..') then
+            continue;
+          if (Rec.Attr and faVolumeID) = faVolumeID then
+            continue; // nothing useful to do with volume IDs
+          if (Rec.Attr and faHidden) = faHidden then
+            continue; // honor the OS "hidden" setting
+          if (Rec.Attr and faDirectory) = faDirectory then
+            continue; // This is a directory. Might want to do something special.
 
-        if Rec.Size = 0 then
-          continue;
+          if Rec.Size = 0 then
+            continue;
 
-        if TempFiles.TryGetValue(Rec.Size, Names) then
-          Names.Add(Rec.Name)
-        else
-        begin
-          Names := TStringList.Create;
-          Names.Add(Rec.Name);
-          TempFiles.Add(Rec.Size, Names)
-        end;
+          if TempFiles.TryGetValue(Rec.Size, Names) then
+            Names.Add(Rec.Name)
+          else
+          begin
+            Names := TStringList.Create;
+            Names.Add(Rec.Name);
+            TempFiles.Add(Rec.Size, Names)
+          end;
 
-      until FindNext(Rec) <> 0;
-    finally
-      FindClose(Rec);
-    end;
+        until FindNext(Rec) <> 0;
+      finally
+        FindClose(Rec);
+      end;
 
-    for FileSize in TempFiles.Keys do
+    for Filesize in TempFiles.Keys do
     begin
-      Names := TempFiles.Items[FileSize];
-      if Names.Count > 1 then // two or more file with same size
+      Names := TempFiles.Items[Filesize];
+      if Names.Count > 1 then // two or more files with same size
       begin
         for I := 0 to Names.Count - 1 do
         begin
           FileInfo.Filename := Names[I];
-          FileInfo.Filesize := IntToStr(FileSize);
+          FileInfo.Filesize := IntToStr(Filesize);
           try
-            FileInfo.Hash := THashSHA2.GetHashStringFromFile(Dir + '\' + FileInfo.Filename);
+            FileInfo.Hash := THashSHA2.GetHashStringFromFile
+              (Dir + '\' + FileInfo.Filename);
           except
             FileInfo.Hash := 'cannot open file';
           end;
@@ -85,7 +87,8 @@ begin
       end;
     end;
   finally
-    for Names in TempFiles.Values do Names.Free;
+    for Names in TempFiles.Values do
+      Names.Free;
     TempFiles.Free;
   end;
 end;
